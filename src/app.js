87a8f8984d43b383caa8c629a502313990d88e65
Icaro Rezende <icaronr@gmail.com>
2019-09-28 10:34:56 -0300

    setup and auth

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
+
+import './database';
 
 class App {
   constructor() {
@@ -11,6 +13,7 @@ class App {
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0c5c7a9977bcb914403889965b3c143b',
+  expiresIn: '7d',
+};
